ca08876788bfd21350185edc545ddeee02b9932b
d33sty <distortion616@gmail.com>
2026-02-02 17:18:59 +0300

    02.02.2026

---
+++ 2.1 Библиотека threading. Поток. Создание потоков. ч.1/e_2.1.5.py
@@ -0,0 +1,21 @@
+import threading
+from time import sleep
+
+
+def task():
+    thread_name = threading.current_thread().name
+    thread_count = threading.active_count()
+    print(
+        f"Задача вызвана потоком {thread_name}, всего активных потоков {thread_count}"
+    )
+    sleep(1)
+    print(f"Задача выполнилась потоком {thread_name}")
+
+
+task()
+new_thread = threading.Thread(target=task)
+new_thread.start()
+new_thread.join()  # <-! блокирует выполнение главного потока
+
+print(f"Количество активных потоков {threading.active_count()}")
+print("END MAIN")
