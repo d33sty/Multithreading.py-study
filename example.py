774948d936ee6bba9a1567c66e589a04dbbdd555
d33sty <distortion616@gmail.com>
2026-02-04 16:34:23 +0300

    04.02.2026

--- example.py
+++ example.py
@@ -1,35 +1,12 @@
-from time import sleep
-from threading import Timer, Thread
-from typing import Callable
+from collections.abc import Callable
+from typing import Any
 
-result = False
-
-
-def task(*args):
-    global result
-    print("task started")
-    sleep(0.5)
-    result = True
-    print("task ended")
+# импортируйте необходимое
 
 
-def callback_task():
-    print("callback")
+class ConcurrentExecutor:
+    ...
 
+    def concurrent_run(self, func: Callable) -> Callable: ...
 
-def callback_handler(
-    task: Callable = None, args=(), callback_task: Callable = None
-) -> None:
-    # не меняйте значение result
-    # дополните код
-    global result
-    thr = Thread(target=task, args=args, daemon=True)
-    thr.start()
-    tim = Timer(0.3 + 0.05, callback_task)
-    tim.start()
-    thr.join(0.3)
-    if not result:
-        tim.cancel()
-
-
-callback_handler(task, tuple(), callback_task)
+    def get_results(self, timeout=None) -> dict[str, Any]: ...
