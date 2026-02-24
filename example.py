3a14fe13f7c1acaf762011bda7c43102333be2af
d33sty <distortion616@gmail.com>
2026-02-24 17:47:25 +0300

    24.02.2026 threading study DONE

--- example.py
+++ example.py
@@ -1,12 +1,1 @@
-from collections.abc import Callable
-from typing import Any
-
-# импортируйте необходимое
-
-
-class ConcurrentExecutor:
-    ...
-
-    def concurrent_run(self, func: Callable) -> Callable: ...
-
-    def get_results(self, timeout=None) -> dict[str, Any]: ...
+# DONE
