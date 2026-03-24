fb36100f9cbcaa14f6e260d30514cbfb0fb77f41
d33sty <distortion616@gmail.com>
2026-03-24 19:20:14 +0300

    23.03.2026

--- async/example.py
+++ async/example.py
@@ -1,26 +1,6 @@
-from time import sleep, perf_counter
-from itertools import count
-from random import randrange
+import socket
 
 
-total_count = count(1)
+my_socket = socket.socket()
 
-
-class Fisherman:
-    def __init__(self, name: str = ""):
-        self.name = name
-
-    def get(self):
-        sleep(randrange(1, 11))
-        print(f"{self.name} поймал одну, всего {next(total_count)}")
-
-
-def main():
-    f1 = Fisherman("Рыбак")
-    #  ???
-
-
-if __name__ == "__main__":
-    start_time = perf_counter()
-    main()
-    print(f"total time {perf_counter() - start_time}")
+print(my_socket.fileno())
